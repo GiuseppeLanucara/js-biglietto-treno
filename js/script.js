6b46befbf8133bf06e259755731d0eabde07256a
GiuseppeLanucara <giuseppelanucara96@gmail.com>
2022-10-16 19:35:20 +0200

    modificato per esercitazione

--- js/script.js
+++ js/script.js
@@ -73,6 +73,16 @@ if (etaPasseggero < 18) {
 }
 
 //OUTPUT valore del biglietto scontato in html
-let html = document.getElementById("out").innerHTML
 
-document.getElementById("out").innerHTML = html + `${bigliettoScontato.toFixed(2)}` + "euro";
+document.getElementById("out").innerHTML =  "Il suo biglietto costa" + ` ${bigliettoScontato.toFixed(2)}` + "euro";
+
+
+
+
+
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
